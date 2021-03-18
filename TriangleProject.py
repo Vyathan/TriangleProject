35ddb016ff631ea71b7faae862e59b8ce74cf87f
Matt Brundage II <mattjbrundage@icloud.com>
2021-03-18 16:21:10 -0400

    Outlined Code

---
+++ TriangleProject.py
@@ -0,0 +1,14 @@
+"""
+Program: TriangleProject.py
+Programmer: Matt Brundage II
+Determine whether or not a triangle is a right triangle.
+Output a statement declaring whether or not the triangle is a right triangle.
+"""
+
+# Declare Contants/Variables
+
+# Request the inputs
+
+# Compute the triangle
+
+# Display the result
